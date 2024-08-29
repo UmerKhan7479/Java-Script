f99c779e2929c82da25ee7ba8a79ee9b699a9509
UmerKhan7479 <162496262+UmerKhan7479@users.noreply.github.com>
2024-08-30 01:31:03 +0500

    Commit

---
+++ Assignment 1/app.js
@@ -0,0 +1,33 @@
+// ASSIGNMENT NO: 01
+
+// Question 1
+
+// Swal.fire({
+//     title: "hello , sir",
+//     width: 600,
+//     padding: "3em",
+//     color: "#716add",
+//     background: "#fff url(/images/trees.png)",
+//     backdrop: `
+//       rgba(0,0,123,0.4)
+//       url("/images/nyan-cat.gif")
+//       left top
+//       no-repeat
+//     `
+//   });
+
+//Question no: 02
+
+// alert("Error! Please a enter a valid password")
+
+//Question no: 03
+
+// alert("Welcome to Js Land..\nHappy Coding!")
+
+//Question No: 04
+// alert("Welcome to Js Land..")
+// alert("Happy Coding!")
+
+//question no:05
+
+// alert("Hello..i can run JS through my web browser's console")
