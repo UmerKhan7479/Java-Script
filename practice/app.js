beece5c04d07bf77e3456845673e8b91ced6179c
UmerKhan7479 <162496262+UmerKhan7479@users.noreply.github.com>
2024-09-29 12:04:15 +0500

    Commit

---
+++ practice/app.js
@@ -0,0 +1,51 @@
+
+// task 1 
+// let str = "hello world umer khan";
+// let capitalizedStr = str.replace(/\b\w/g, char => char.toUpperCase());
+
+// console.log(capitalizedStr);
+
+//task 2 
+
+// var given_prompt = prompt("What do you want to find?").toLowerCase(); 
+// var given_sentence = "hello, i am saylani mass it training student";
+
+// if (given_sentence.includes(given_prompt)) {
+//     alert("The sentence contains the word '" + given_prompt + "'");
+// } else {
+//     alert("The sentence does not contain the word '" + given_prompt + "'");
+// }
+
+// var given_prompt = prompt("What do you want to find?").toLowerCase(); 
+//         var given_sentence = "hello, i am saylani mass it training student".toLowerCase();
+
+//         var found = false;
+
+       
+//         var words = given_sentence.split(" ");
+
+//         for (var i = 0; i < words.length; i++) {
+//             var word = words[i].slice(0); 
+//             if (word === given_prompt) {
+//                 found = true;
+//                 break;
+//             }
+//         }
+
+//         if (found) {
+//             alert("The webpage contains the word '" + given_prompt + "'");
+//         } else {
+//             alert("The webpage does not contain the word '" + given_prompt + "'");
+//         }
+
+// task 3
+
+// var given_sentence = "hello, i am saylani mass it training student";
+// var index_saylani = given_sentence.indexOf("saylani")
+// console.log(index_saylani)
+
+//task 4
+
+var given_sentence = "hello, i am saylani mass it training student";
+var replace_saylani = given_sentence.replace("saylani","Model")
+console.log(index_saylani)
